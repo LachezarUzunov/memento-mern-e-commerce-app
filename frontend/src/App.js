6d2a728254308bcc70e18d5e942231e867f66ebf
lachezaruzunov <uzunov_89_lacho@abv.bg>
2022-09-17 17:13:55 +0300

    Adding home screen and product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <Fragment>
-      <h1>Welcome to our shop!</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </Fragment>
   );
 }
