810271f8a70f07a06afe556fb224be9a0231bdc1
lachezaruzunov <uzunov_89_lacho@abv.bg>
2022-09-27 10:23:15 +0300

    Adding Redux and fetching products from DB with it

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { productListReducer } from "./reducers/productReducers";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    productList: productListReducer,
+  },
 });
 
 export default store;
