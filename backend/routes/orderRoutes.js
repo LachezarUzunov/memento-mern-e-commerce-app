95d8918f7d83f3900787cc3a6f476bcdf2687c12
lachezaruzunov <uzunov_89_lacho@abv.bg>
2022-11-09 07:19:12 +0200

    Adding backend update order to paid route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require("../controllers/orderController");
 const protect = require("../middleware/authmiddleware");
 
 router.post("/", protect, addOrderItems);
 router.get("/:id", protect, getOrderById);
+router.put("/:id/pay", protect, updateOrderToPaid);
 
 module.exports = router;
